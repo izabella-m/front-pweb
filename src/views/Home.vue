c39402c0382fbeaf285254eb7c00f2e965e23ad9
Katarina <kas10@aluno.ifal.edu.br>
2024-06-27 13:57:24 -0300

    estilizando tela de início

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,36 @@
 <template>
-  <LandingPage />
+  <div class="hello">
+
+    <h3> LINK PARA O REPOSITÓRIO</h3>
+    <ul>
+      <li><a href="https://github.com/izabella-m/front-pweb" target="_blank" rel="noopener">LUMIERE</a></li>
+    </ul>
+  </div>
 </template>
 
-<script setup>
-  import LandingPage from '@/components/LandingPage.vue'
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  }
+}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
