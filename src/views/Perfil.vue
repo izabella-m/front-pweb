424753e5e4a38a556dbfee291d8f70e215afedfc
Katarina <kas10@aluno.ifal.edu.br>
2024-06-28 14:18:51 -0300

    adicionando foto de perfil

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -9,6 +9,11 @@
           <v-card-text>
             <v-container>
               <v-row>
+                <v-col cols="12" class="d-flex justify-center">
+                  <v-avatar size="100">
+                    <v-img :src="avatarUrl" />
+                  </v-avatar>
+                </v-col>
                 <v-col cols="12">
                   <v-text-field v-model="name" label="Nome" readonly></v-text-field>
                   <v-text-field v-model="email" label="Email" readonly></v-text-field>
@@ -25,9 +30,11 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { getAuth } from 'firebase/auth';
+import defaultAvatar from '@/assets/avatar.jpg'
 
 const name = ref('');
 const email = ref('');
+const avatarUrl = ref(defaultAvatar);
 
 onMounted(() => {
   const auth = getAuth();
@@ -35,6 +42,7 @@ onMounted(() => {
   if (user) {
     email.value = user.email;
     name.value = user.displayName || '';
+    avatarUrl.value = user.photoURL || defaultAvatar;
   } else {
     console.error("User not logged in");
   }
