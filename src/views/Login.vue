654e942a399fc1842b60b42f6d52624e84c71aa4
izabella-m <izabella.pessoa03@outlook.com>
2024-07-21 20:33:08 -0300

    Style for login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,57 @@
 <template>
-  <h1> Sign In to an Account</h1>
-  <p><input type="text" placeholder="Email" v-model="email"/></p>
-  <p><input type="password" placeholder="Password" v-model="password"/></p>
-  <p v-if="errMsg">{{ errMsg }}</p>
-  <p><button @click="register">Entrar</button></p>
-  <p><button @click="signInWithGoogle">Entre com sua conta do Google</button></p>
+  <v-app style="background-color: #000000">
+    <v-container class="fill-height mx-auto justify-center">
+      <v-responsive class="align-center text-center fill-height">
+        <h1 class="titleLoginPage">Bem-vindo de volta</h1>
+        <p class="textDescriptionLogin mb-5">Entre para explorar e continue sua jornada pelo mundo dos filmes</p>
+        
+        <v-text-field 
+          variant="underlined" 
+          type="text" 
+          placeholder="Email" 
+          v-model="email"
+          class="mx-auto justify-center emailField" 
+        />
+
+
+        <v-text-field 
+          variant="underlined" 
+          class="mx-auto justify-center passwordField mb-10" 
+          type="password" 
+          placeholder="Password" 
+          v-model="password"
+        />      
+        
+        <p v-if="errMsg">{{ errMsg }}</p>
+
+        <v-btn 
+          @click="register"
+          class="btnLogin"  
+          rounded 
+          width="280"> 
+          <p class="btnLoginText">Entre</p>
+        </v-btn>
+
+        <v-container class="d-flex distancerDivider">
+          <v-divider class="mx-auto ml-10" :thickness="3" color="white" width="10"></v-divider>
+            <p class="mt-n2 mx-3" style="color: white; font-size: 12px">ou</p>
+          <v-divider class="mx-auto mr-10" :thickness="3" color="white" width="10"></v-divider>
+        </v-container>
+
+        <v-btn 
+          @click="signInWithGoogle"
+          class="btnLogin"  
+          rounded 
+          width="280"> 
+            <p class="btnLoginText">Entre com sua conta do Google</p>
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <p class="registerTextRoute mt-10">Ainda não tem uma conta?<v-btn width="4" variant="icon" class="LoggedTextClick mb-1 ml-2" @click="goToRegister"><p style="font-size: 12px; color: #ffffff; text-transform: initial">Cadastre-se</p></v-btn></p>
+      </v-responsive>
+    </v-container>
+  </v-app>
 </template>
 
 <script setup>
@@ -59,4 +106,62 @@ const signInWithGoogle = () => {
       errMsg.value = "Erro ao fazer login com o Google";
     });
 };
+
+function goToRegister() {
+    router.push('/register');
+  }
 </script>
+
+<style>
+
+.fill-height {
+  max-width: 380px;
+}
+
+.titleLoginPage {
+  font-family: "Anton";
+  font-size: 35px;
+  font-weight: 200;
+  background: linear-gradient(90deg, #2203FF, #C40D60);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.textDescriptionLogin {
+  color: white;
+  font-family: Inter;
+}
+
+.emailField {
+  color: #ffffff;
+  max-width: 350px;
+}
+
+
+.passwordField {
+  color: rgb(255, 255, 255);
+  max-width: 350px;
+}
+
+.btnLogin {
+  background: linear-gradient(to right, #2203FF, #C40D60) !important;
+  color: white;
+}
+
+.btnLoginText {
+  color: #ffffff;
+  font-weight: 500; 
+  font-size: 12px; 
+  font-family: poppins;
+  text-transform: initial;
+}
+
+.distancerDivider {
+  margin-left: 10px;
+}
+
+.registerTextRoute {
+  color: white;
+  font-size: 12px;
+}
+</style>
